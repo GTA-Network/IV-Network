2ae6466c248eec9c647110f82d96c90632e59a5a
Alexander Güttler <x-force@hotmail.de>
2013-08-09 14:53:57 +0200

    Added some cpp files for ResourceSystem

---
+++ Shared/Scripting/ResourceSystem/CIncludedResource.cpp
@@ -0,0 +1,10 @@
+//============== IV: Multiplayer - http://code.iv-multiplayer.com ==============
+//
+// File: CIncludedResource.cpp
+// Project: Shared
+// Author: xForce <xf0rc3.11@gmail.com>
+// License: See LICENSE in root directory
+//
+//==============================================================================
+
+#include "CIncludedResource.h"
---
+++ Shared/Scripting/ResourceSystem/CResourceServerScript.cpp
@@ -0,0 +1,10 @@
+//============== IV: Multiplayer - http://code.iv-multiplayer.com ==============
+//
+// File: CResourceServerScript.cpp
+// Project: Shared
+// Author: xForce <xf0rc3.11@gmail.com>
+// License: See LICENSE in root directory
+//
+//==============================================================================
+
+#include "CResourceServerScript.h"
