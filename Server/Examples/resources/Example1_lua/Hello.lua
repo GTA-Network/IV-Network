e3c5cf411679200d01f89eb0ad00a4f2cb9d2101
Sebihunter <sebihunter@hotmail.com>
2013-08-13 15:46:42 +0200

    Removed unnecessary semicolons from lua example

--- Server/Examples/resources/Example1_lua/Hello.lua
+++ Server/Examples/resources/Example1_lua/Hello.lua
@@ -1,9 +1,9 @@
-local inst = testClass();
-inst:setPosition(1, 1, 1);
-inst:setPosition(2.0, 2.0, 2.0);
-inst:setPosition3(3.0, 3.0, 3.0);
+local inst = testClass()
+inst:setPosition(1, 1, 1)
+inst:setPosition(2.0, 2.0, 2.0)
+inst:setPosition3(3.0, 3.0, 3.0)
 
 
-collectgarbage();
-inst = nil;
+collectgarbage()
+inst = nil
 
