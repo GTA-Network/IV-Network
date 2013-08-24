871e448fe6a153ff5ebfaddeedb1e50a8f2a6a8a
FRiChicken <Martinjk@outlook.com>
2013-08-24 21:56:46 +0200

    Cleaned up directory structure

--- Binary/resources/Example1_squirrel/Hello.nut
+++
@@ -1,9 +0,0 @@
-function wantEventCallback()
-{
-	print("I want COOKIES!");
-}
-addGlobalEvent("wantEvent", wantEventCallback);
-
-// calls event in Hello.lua
-triggerGlobalEvent("testEvent");
-
--- Files/GetReady.bat
+++
@@ -1,4 +0,0 @@
-xcopy /s /y "..\Binary\IVMultiplayer.dll" "%~dp0"
-xcopy /s /y "..\Binary\IVGameReady.exe" "%~dp0"
-xcopy /s /y "..\Binary\LaunchIVMultiplayer.exe" "%~dp0"
-xcopy /s /y "..\Binary\IVMultiplayerLaunchInjector.dll" "%~dp0"
