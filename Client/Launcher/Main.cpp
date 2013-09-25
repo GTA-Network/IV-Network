beeaf0be5346c93ffa32657680e604241caa509f
ViruZz <viruzmallett@gmail.com>
2013-09-25 12:59:22 -0400

    Fixed a compiling error in Server.Core
    Cleaned some code

--- Client/Launcher/Main.cpp
+++ Client/Launcher/Main.cpp
@@ -173,7 +173,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// Check if we have an server connect command
 	CString strServer, strPort;
 	std::string strServerCheck = CString(lpCmdLine);
-	std::size_t sizetCMDFound = strServerCheck.find(SHORT_COMMANDLINE_LAUNCH_1);// -[1]i[2]v[3]m[4]p[5]*space*[6]***.***.***.***
+	std::size_t sizetCMDFound = strServerCheck.find(SHORT_COMMANDLINE_LAUNCH_1);
 	int iOffset = 0;
 	bool bCommandFound = false;
 	CString strNewCommandLine = lpCmdLine;
@@ -186,7 +186,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	// Check for ivmp protocol
 	if(!bCommandFound) {
-		sizetCMDFound = strServerCheck.find(SHORT_URI_LAUNCH_1); // i[1]v[2]m[3]p[4]:[5]/[6]/[7]***.***.***
+		sizetCMDFound = strServerCheck.find(SHORT_URI_LAUNCH_1); 
 		if(sizetCMDFound != std::string::npos) {
 			iOffset = 7;
 			bCommandFound = true;
@@ -195,7 +195,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	// Check for ivmultiplayer protocol
 	if(!bCommandFound) {
-		sizetCMDFound = strServerCheck.find(SHORT_URI_LAUNCH_2);// i[1]v[2]m[3]u[4]l[5]t[6]i[7]p[8]l[9]a[10]y[11]e[12]r[13]:[14]/[15]/[16]***.***.***
+		sizetCMDFound = strServerCheck.find(SHORT_URI_LAUNCH_2);
 		if(sizetCMDFound != std::string::npos) {
 			iOffset = 16;
 			bCommandFound = true;
@@ -240,7 +240,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			else // Terminate IV:MP
 			{
 				if(!SharedUtility::_TerminateProcess(MP_START_EXECUTABLE))
-					return ShowMessageBox(MP_START_EXECUTABLE" could not be terminated. Cannot launch IV: Multiplayer.");
+					return ShowMessageBox(MP_START_EXECUTABLE" could not be terminated. Cannot launch IV:Network.");
 			}
 
 		}
--- Server/CServer.h
+++ Server/CServer.h
@@ -52,10 +52,10 @@ private:
 	CNetworkModule				* m_pNetworkModule;
 
 
-	unsigned int				m_uiSyncRate = 20; /** Describes the maximum number of sync packages sent per second per entity */
-	unsigned int				m_uiMaximumFPS = 100;
+	unsigned int                m_uiSyncRate;// Seems VS2013 sometimes does not allow C++11 = 20; /** Describes the maximum number of sync packages sent per second per entity */
+    unsigned int                m_uiMaximumFPS;// Seems VS2013 sometimes does not allow C++11 = 100;
 
-	bool						m_bShowFPS = true;
+    bool                        m_bShowFPS;// Seems VS2013 sometimes does not allow C++11 = true;
 	unsigned long				m_ulLastFPSUpdateTime;
 	unsigned long				m_ulFrameCount;
 	unsigned long				m_ulFramesPerSecond;
