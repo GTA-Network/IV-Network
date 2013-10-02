c9f8ca46dee5d1185adb653aa436f491338b3dfd
Knightz <pc_jet@abv.bg>
2013-10-02 19:51:57 +0300

    Fixed the IVMapEditor.exe build error

---
+++ Client/IVMapEditor/Main.cpp
@@ -0,0 +1,16 @@
+//================= IV:Network - https://github.com/GTA-Network/IV-Network =================
+//
+// File: Main.cpp
+// Project: IVMapEditor
+// License: See LICENSE in root directory
+//
+//==========================================================================================
+
+#include <ShlObj.h>
+#include <mmsystem.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+
+	return 0;
+}
