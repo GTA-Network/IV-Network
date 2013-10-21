a17d13f21bf9a47abbda618b4aa96b68d65e8e38
Knightz <pc_jet@abv.bg>
2013-10-21 19:24:17 +0300

    Cleaned up some code

--- Server/Scripting/Natives/CScriptClasses.cpp
+++ Server/Scripting/Natives/CScriptClasses.cpp
@@ -98,87 +98,10 @@ void CScriptClasses::Register(CScriptVM * pVM)
 		AddMethod("setPosition", &CScriptVehicle::SetPosition).
 		AddMethod("setRotation", &CScriptVehicle::SetRotation).
 		AddMethod("setMoveSpeed", &CScriptVehicle::SetMoveSpeed).
-		AddMethod("setTurnSpeed", &CScriptVehicle::SetTurnSpeed).
 		//AddMethod("getHealth", &CScriptVehicle::GetHealth).
 		//AddMethod("setHealth", &CScriptVehicle::SetHealth).
 		Register(pVM);
 	}
-}
 
 
-int CScriptClasses::CreateEntity(int * VM)
-{
-	CResource* pResource = CResourceManager::GetInstance()->Get(VM);
-	
-	if(pResource)
-	{
-		CScriptVM* pVM = pResource->GetVM();
-		
-		CString strEntity = "HJHKJ";
-		/*pVM->Pop(strEntity);
-		pVM->ResetStackIndex();*/
-		if(strEntity == "3DLABEL")
-		{
-			C3DLabelEntity* p3DLabel = new C3DLabelEntity();
-			pVM->SetClassInstance("C3DLabelEntity", (void*)p3DLabel);
-			CEntityNatives::Register(pVM);
-			C3DLabelNatives::Register(pVM);
-		} else if(strEntity == "ACTOR") {
-			CActorEntity* pActor = new CActorEntity();
-			pVM->SetClassInstance("CActorEntity", (void*)pActor);
-			CEntityNatives::Register(pVM);
-			CActorNatives::Register(pVM);
-		} else if(strEntity == "BLIP") {
-			CBlipEntity * pBlip = new CBlipEntity();
-			pVM->SetClassInstance("CBlipEntity", (void*)pBlip);
-			CEntityNatives::Register(pVM);
-			CBlipNatives::Register(pVM);
-		} else if(strEntity == "CHECKPOINT") {
-			CCheckpointEntity* pCheckpoint = new CCheckpointEntity();
-			pVM->SetClassInstance("CCheckpointEntity", (void*)pCheckpoint);
-			CEntityNatives::Register(pVM);
-			CCheckpointNatives::Register(pVM);
-		} else if(strEntity == "FIRE") {
-			CFireEntity* pFire = new CFireEntity();
-			pVM->SetClassInstance("CFireEntity", (void*)pFire);
-			CEntityNatives::Register(pVM);
-			CFireNatives::Register(pVM);
-		} else if(strEntity == "OBJECT") {
-			CObjectEntity* pObject = new CObjectEntity();
-			pVM->SetClassInstance("CObjectEntity", (void*)pObject);
-			CEntityNatives::Register(pVM);
-			CObjectNatives::Register(pVM);
-		} else if(strEntity == "PICKUP") {
-			CPickupEntity* pPickup = new CPickupEntity();
-			pVM->SetClassInstance("CPickupEntity", (void*)pPickup);
-			CEntityNatives::Register(pVM);
-			CPickupNatives::Register(pVM);
-		} else if(strEntity == "PLAYER") {
-			CPlayerEntity* pPlayer = new CPlayerEntity();
-			pVM->SetClassInstance("CPlayerEntity", (void*)pPlayer);
-			CEntityNatives::Register(pVM);
-			CPlayerNatives::Register(pVM);
-		} else /*if(strEntity == "VEHICLE")*/ {
-			CVehicleEntity* pVehicle = new CVehicleEntity();
-
-			/* now read the additional params for vehicle spawning */
-
-			// Memo: add params count check in VM Pop methods
-			// Merge the pop events so we use only Pop instead of PopVector
-			//CVector3 vecPos;
-			//CVector3 vecRot;
-			//pVM->Pop(vecPos);
-			//pVM->Pop(vecRot);
-
-			//// Do we need the id; maybe internal for easier sync but definetly not public to the scripting engine
-			//pVehicle->SetId(CServer::GetInstance()->GetVehicleManager()->Add(pVehicle));
-			//
-			//pVM->SetClassInstance("CVehicleEntity", (void*)pVehicle);
-			//CEntityNatives::Register(pVM);
-			//CVehicleNatives::Register(pVM);
-
-			//pVehicle->Spawn();
-		}
-	}
-	return 1;
 }
