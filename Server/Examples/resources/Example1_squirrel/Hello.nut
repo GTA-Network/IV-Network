1c5dae9af4d315122bf5f648200ab41d2f3510c4
g3o0or <baisangurov.geor@mail.ru>
2014-02-24 00:04:42 +0400

    standart instant messages deleted, use sendMessageToAll(player.getName()+"("+player.getId()+"): "+text, 0xFFFFFF, true);

--- Server/Examples/resources/Example1_squirrel/Hello.nut
+++ Server/Examples/resources/Example1_squirrel/Hello.nut
@@ -7,3 +7,9 @@ addGlobalEvent("wantEvent", wantEventCallback);
 // calls event in Hello.lua
 triggerGlobalEvent("testEvent");
 
+function onPlayerChat(text, player)
+{
+	sendMessageToAll(player.getName()+"("+player.getId()+"): "+text, 0xFFFFFF, true);
+}
+
+addEvent("playerChat", onPlayerChat);
--- Server/Network/CNetworkRPC.cpp
+++ Server/Network/CNetworkRPC.cpp
@@ -263,14 +263,14 @@ void PlayerChat(RakNet::BitStream * pBitStream, RakNet::Packet * pPacket)
 			args.push(strInput);
 			args.push(pPlayer->GetScriptPlayer());
 			CEvents::GetInstance()->Call((!bIsCommand) ? ("playerChat") : ("playerCommand"), &args, CEventHandler::eEventType::NATIVE_EVENT, nullptr);
-			if (!bIsCommand)
+/*			if (!bIsCommand)
 			{
 				// Send the RPC back to other players
 				RakNet::BitStream bitStream;
 				bitStream.Write(playerId);
 				bitStream.Write(strInput);
 				CServer::GetInstance()->GetNetworkModule()->Call(GET_RPC_CODEX(RPC_PLAYER_CHAT), &bitStream, HIGH_PRIORITY, RELIABLE_ORDERED, -1, true);
-			}
+			}*/
 		}
 	}
 }
